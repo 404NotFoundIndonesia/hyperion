8c52b3f86569eb4bc3d39be87dd144d3412bc783
Andika Sujanadi <andikasujanadi@gmail.com>
2025-03-15 14:40:30 +0800

    #14 help menu is functioning

--- frontend/src/menuConfig.js
+++ frontend/src/menuConfig.js
@@ -1,9 +1,10 @@
+// @ts-nocheck
 
-import { 
-  openFiles, 
-  openFolder, 
-  exportFiles, 
-  removeAllFiles, 
+import {
+  openFiles,
+  openFolder,
+  exportFiles,
+  removeAllFiles,
   unloadSelectedFile,
   obfuscateAll,
   obfuscateSelectedFile,
@@ -11,31 +12,38 @@ import {
 } from "./appActions.js";
 
 export const menuConfig = [
-    {
-        name: "File",
-        children: [
-          { name: "Open", shortcut: "Ctrl+O", action: async () => await openFiles() }, // Ensure async execution
-          { name: "Open Folder", shortcut: "Ctrl+Shift+O", action: async () => await openFolder()},
-          { name: "Save", shortcut: "Ctrl+S", action: () => console.log("Save") },
-          { name: "Save As", shortcut: "Ctrl+Shift+S", action: async () => await exportFiles() },
-          { name: "Close", shortcut: "Ctrl+W", action: () => unloadSelectedFile() },
-          { name: "Close Folder", shortcut: "Ctrl+Shift+W", action: () => removeAllFiles() }
-        ]
+  {
+    name: "File",
+    children: [
+      { name: "Open", shortcut: "Ctrl+O", action: async () => await openFiles() }, // Ensure async execution
+      { name: "Open Folder", shortcut: "Ctrl+Shift+O", action: async () => await openFolder() },
+      { name: "Save", shortcut: "Ctrl+S", action: () => console.log("Save") },
+      { name: "Save As", shortcut: "Ctrl+Shift+S", action: async () => await exportFiles() },
+      { name: "Close", shortcut: "Ctrl+W", action: () => unloadSelectedFile() },
+      { name: "Close Folder", shortcut: "Ctrl+Shift+W", action: () => removeAllFiles() }
+    ]
+  },
+  {
+    name: "Run",
+    children: [
+      { name: "Obfuscate", action: async () => await obfuscateSelectedFile() },
+      { name: "Obfuscate All", action: async () => await obfuscateAll() },
+      { name: "Configuration", action: () => toggleConfig() },
+    ]
+  },
+  {
+    name: "Help",
+    children: [
+      {
+        name: "How to Contribute", action: () => window.runtime.BrowserOpenURL(
+          "https://github.com/404NotFoundIndonesia/hyperion",
+        )
       },
-    {
-      name: "Run",
-      children: [
-        { name: "Obfuscate", action: async () => await obfuscateSelectedFile() },
-        { name: "Obfuscate All", action: async () => await obfuscateAll() },
-        { name: "Configuration", action: () => toggleConfig() },
-      ]
-    },
-    {
-      name: "Help",
-      children: [
-        { name: "How to Contribute", action: () => console.log("How to Contribute") },
-        { name: "Report Issue", action: () => console.log("Report Issue") }
-      ]
-    }
-  ];
-  
+      {
+        name: "Report Issue", action: () => window.runtime.BrowserOpenURL(
+          "https://github.com/404NotFoundIndonesia/hyperion/issues",
+        )
+      }
+    ]
+  }
+];
